d4eaa159de992a5ecef2a22718c407793fe3fa4c
abbjesjan <43879117+abbjesjan@users.noreply.github.com>
2019-04-21 12:09:13 +0200

    uppdatering 7.1
    bättre läsbarhet

--- kap_7/7.1.py
+++ kap_7/7.1.py
@@ -1,23 +1,24 @@
-valt_nummer = 0 
+valt_nummer = 0
 summa = 0
 antal_gånger = 1
 
 try:
-    valt_nummer = int(input("Ange en multiplikationstabell>>>")) #kollar så att inputen är ett heltal
+    # kollar så att inputen är ett heltal
+    valt_nummer = int(input("Ange en multiplikationstabell>>>"))
 except ValueError:
-    antal_gånger = 0 #sätter till 0 om inputen inte är av int-typ
+    antal_gånger = 0  # sätter till 0 om inputen inte är av int-typ
 
-if antal_gånger !=0: #kör enbart while-loopen om ett korekkt tal skrvits in
+if antal_gånger != 0:  # kör enbart while-loopen om ett korekkt tal skrvits in
     while antal_gånger <= 3:
-        summa += valt_nummer #adderar med den valda tabellen
-        print(summa) #skriver summan
-    
+        summa += valt_nummer  # adderar med den valda tabellen
+        print(summa)  # skriver summan
 
         if antal_gånger == 3:
-            fortsätta = input("Vill du fortsätta? (ja/nej)").lower() #frågar efter 3 tal om användaren vill fortsätta
+            # frågar efter 3 tal om användaren vill fortsätta
+            fortsätta = input("Vill du fortsätta? (ja/nej)").lower()
             if fortsätta == "ja":
-                antal_gånger = 1 # "nollställer" antalet gånger om svaret är ja
-                continue #hoppar över sista koden så att antal_gånger = 1 vid while-loopens början
+                antal_gånger = 1  # "nollställer" antalet gånger om svaret är ja
+                continue  # hoppar över sista koden så att antal_gånger = 1 vid while-loopens början
             elif fortsätta == "nej":
                 break
             else:
@@ -25,4 +26,4 @@ if antal_gånger !=0: #kör enbart while-loopen om ett korekkt tal skrvits in
 
         antal_gånger += 1
 else:
-    print("Ej korrekt inmatad maultiplikationstabell.") # felmeddelande
+    print("Ej korrekt inmatad maultiplikationstabell.")  # felmeddelande
